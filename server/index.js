f16dbb5f30cc758d7ef4eb8598b935a0792602cd
prajwal160 <alaladinniprajwal@gmail.com>
2023-08-21 17:14:42 +0530

    Added/Updated import statements for routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import  mongoose  from "mongoose";
 
+import userRoutes from './routes/users.js'; 
+import questionRoutes from './routes/questions.js';
+import answerRoutes from  './routes/answers.js';
+
 const app = express();
 dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -14,14 +18,13 @@ app.get('/',(req, res) => {
   res.send("hlo prajwal how are you")
 })
 
-app.use('/users', userRoutes) 
-app.use('/questions', questionsRoutes) 
+app.use('/user', userRoutes) 
+app.use('/questions', questionRoutes) 
 app.use('/answer', answerRoutes) 
 
 const PORT = process.env.PORT || 5000
 
 const DATABASE_URL= process.env.CONNECTION_URL
-//"mongodb+srv://prajwal:prajwal@clone.q0aob4q.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect( DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)})) 
