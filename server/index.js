d6a6fb893b35f3acb4a9a0356e3a1d50bf3e0957
prajwal160 <alaladinniprajwal@gmail.com>
2023-08-22 14:07:13 +0530

    updated the filename

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import  mongoose  from "mongoose";
 
 import userRoutes from './routes/users.js'; 
 import questionRoutes from './routes/Questions.js';
-import answerRoutes from  './routes/answers.js';
+import answerRoutes from  './routes/Answers.js';
 
 const app = express();
 dotenv.config();
